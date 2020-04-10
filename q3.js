cff99822faee51cb25dccccba68aa10d68c606e9
rijan <hrijon.chp@gmail.com>
2020-04-10 08:50:02 +0800

    identify num of links and forms in html page

--- q3.js
+++ q3.js
@@ -1,6 +1,4 @@
-
   var d = new Date();
-
   var day = d.getDay().toString(10);
   var date = d.getDate().toString(10);
   var month = d.getMonth().toString(10);
@@ -12,3 +10,15 @@
                 year.fontcolor("blue") + "&nbsp; &nbsp;"  +  hours.fontcolor("brown") + ":"  + minutes.fontcolor("brown");
   
   document.getElementById("Result").innerHTML = result;
+                                            
+
+
+  var x = "User-agent header Information: " + navigator.userAgent;
+  document.getElementById("bInfo").innerHTML = x;
+
+
+  var nLinks = document.links.length;
+  document.getElementById("numLinks").innerHTML = nLinks;
+
+  var nForms = document.forms.length;
+  document.getElementById("numForms").innerHTML = nForms;
